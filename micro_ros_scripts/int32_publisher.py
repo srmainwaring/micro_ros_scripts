8a08517825be97f4ddc4795d0b6e39e2a13d9ef0
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-07-17 09:24:06 +0100

    Update int32_publisher example
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- micro_ros_scripts/int32_publisher.py
+++ micro_ros_scripts/int32_publisher.py
@@ -1,14 +1,34 @@
+"""
+Usage
+
+Run agent:
+micro_ros_ws % ros2 run micro_ros_agent micro_ros_agent udp4 --port 2019 --verbose 6
+
+Run client:
+RMW_IMPLEMENTATION=rmw_microxrcedds ros2 run micro_ros_demos_rclc int32_subscriber
+
+Run publisher:
+ros2 run micro_ros_scripts int32_publisher --ros-args --param rate:=10.0
+"""
+
 import rclpy
+
 from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor
 
 from std_msgs.msg import Int32
 
 
 class Int32Publisher(Node):
-    def __init__(self):
+    def __init__(self, rate):
         super().__init__("int32_publisher")
+
+        parameter_descriptor = ParameterDescriptor(description="Publish rate (Hz)")
+        self.declare_parameter("rate", 1.0, parameter_descriptor)
+        self.rate = self.get_parameter("rate").get_parameter_value().double_value
+
         self.publisher_ = self.create_publisher(Int32, "/std_msgs_msg_Int32", 10)
-        timer_period = 0.5  # seconds
+        timer_period = 1.0 / self.rate  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
@@ -16,14 +36,14 @@ class Int32Publisher(Node):
         msg = Int32()
         msg.data = self.i
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info("Publishing: {}".format(msg))
         self.i += 1
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    int32_publisher = Int32Publisher()
+    int32_publisher = Int32Publisher(rate=1000)
 
     rclpy.spin(int32_publisher)
 
